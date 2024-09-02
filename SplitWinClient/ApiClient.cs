c1f890c31380a7d9208dfffc9fff4a350dd253bc
levzernov <lev.zernov@yandex.ru>
2024-09-02 16:36:58 +0300

    code review

--- SplitWinClient/ApiClient.cs
+++ SplitWinClient/ApiClient.cs
@@ -1,85 +1,83 @@
-﻿namespace SplitWinClient
+﻿namespace SplitWinClient;
+
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+public class ApiClient
 {
-    using System;
-    using System.Net.Http;
-    using System.Net.Mime;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Windows;
+    private readonly HttpClient _httpClient;
+    private readonly ApiClientLogger _logger;
 
-    public class ApiClient              
+    public ApiClient()
     {
-        private readonly HttpClient _httpClient;        
-        private readonly ApiClientLogger _logger;
+        _httpClient = new HttpClient();
+        _logger = new ApiClientLogger();
 
-        public ApiClient()              
+        // TODO NO HARDCODE
+        _httpClient.BaseAddress = new Uri("http://localhost:5125/");
+    }
+
+    public async Task<string> GetAsync(string url)
+    {
+        try
         {
-            _httpClient = new HttpClient();                     
-            _logger = new ApiClientLogger();
-
-            // TODO NO HARDCODE
-            _httpClient.BaseAddress = new Uri("http://localhost:5125/");
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
         }
-
-        public async Task<string> GetAsync(string url)
+        catch (HttpRequestException ex)
         {
-            try
-            {
-                var response = await _httpClient.GetAsync(url);
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadAsStringAsync();
-            }
-            catch (HttpRequestException ex)
-            {
-                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                return null;
-            }
+            MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            return null;
         }
+    }
 
-        public async Task<string> PostAsync(string url, string json)
+    public async Task<string> PostAsync(string url, string json)
+    {
+        try
         {
-            try
-            {
-                var response = await _httpClient.PostAsync(url, new StringContent(json, Encoding.UTF8, "text/json"));
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadAsStringAsync();
-            }
-            catch (HttpRequestException ex)
-            {
-                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                return null;
-            }
+            var response = await _httpClient.PostAsync(url, new StringContent(json, Encoding.UTF8, "text/json"));
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
         }
-
-        //public async Task<string> PutAsync(string url, string json)
-        //{
-        //    try
-        //    {
-        //        var content = new StringContent(json, Encoding.UTF8, "application/json");
-        //        var response = await _httpClient.PutAsync(url, content);
-        //        response.EnsureSuccessStatusCode();
-        //        return await response.Content.ReadAsStringAsync();
-        //    }
-        //    catch (HttpRequestException ex)
-        //    {
-        //        MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-        //        return null;
-        //    }
-        //}
-
-        public async Task<string> DeleteAsync(string url)
+        catch (HttpRequestException ex)
         {
-            try
-            {
-                var response = await _httpClient.DeleteAsync(url);
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadAsStringAsync();
-            }
-            catch (HttpRequestException ex)
-            {
-                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                return null;
-            }
+            MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            return null;
+        }
+    }
+
+    //public async Task<string> PutAsync(string url, string json)
+    //{
+    //    try
+    //    {
+    //        var content = new StringContent(json, Encoding.UTF8, "application/json");
+    //        var response = await _httpClient.PutAsync(url, content);
+    //        response.EnsureSuccessStatusCode();
+    //        return await response.Content.ReadAsStringAsync();
+    //    }
+    //    catch (HttpRequestException ex)
+    //    {
+    //        MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+    //        return null;
+    //    }
+    //}
+
+    public async Task<string> DeleteAsync(string url)
+    {
+        try
+        {
+            var response = await _httpClient.DeleteAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        catch (HttpRequestException ex)
+        {
+            MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            return null;
         }
     }
 }
--- SplitWinClient/ApiClientLogger.cs
+++ SplitWinClient/ApiClientLogger.cs
@@ -1,24 +1,21 @@
-﻿namespace SplitWinClient
+﻿namespace SplitWinClient;
+
+using System;
+using System.Diagnostics;
+
+using Microsoft.Extensions.Logging;
+internal class ApiClientLogger : ILogger
 {
-    using System;
-    using System.Diagnostics;
-
-    using Microsoft.Extensions.Logging;
-    internal class ApiClientLogger : ILogger
+    public void Log<TState>(LogLevel logLevel,
+                            EventId eventId,
+                            TState state,
+                            Exception? exception,
+                            Func<TState, Exception?, string> formatter)
     {
-        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
-        {
-            Debug.WriteLine($"{logLevel}, {eventId}, {state}, {exception}, {formatter}");
-        }
-
-        public bool IsEnabled(LogLevel logLevel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
-        {
-            throw new NotImplementedException();
-        }
+        Debug.WriteLine($"{logLevel}, {eventId}, {state}, {exception}, {formatter}");
     }
+
+    public bool IsEnabled(LogLevel logLevel) => throw new NotImplementedException();
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull => throw new NotImplementedException();
 }
